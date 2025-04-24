e55b77c01344489cef56745bc8546c353676253a
N0tZetsu <emamainn@gmail.com>
2025-04-24 16:06:22 -0300

    Add esx_hud/fxmanifest.lua 1745521579185

---
+++ esx_hud/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BLACKBOXAI'
+description 'ESX HUD NUI showing job and money metrics with SVG icons'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
